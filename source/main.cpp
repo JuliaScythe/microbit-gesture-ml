c32f6bac29e7fd04c15c8cf9e1d2ed5d4f1fa1c2
Julia <jlavenderpanic@gmail.com>
2023-04-03 14:05:56 +0100

    Initial commit

--- source/main.cpp
+++ source/main.cpp
@@ -1,13 +1,70 @@
 #include "MicroBit.h"
-#include "samples/Tests.h"
 
 MicroBit uBit;
+MicroBitDisplay *display;
+
+double smoothedX;
+double smoothedY;
+double smoothedZ;
+
+// Should be kept in sync with the ml-machine settings
+const int numSamples = 60;
+const int samplesPerSecond = 4;
+
+// We'll use these as a circular buffer to store past data
+int prevDataBufferHead = 0; // the index of the next point to write data to
+double prevDataX[numSamples];
+double prevDataY[numSamples];
+double prevDataZ[numSamples];
+
+/*
+    Converts a 0-60 index into the index space of the circular buffer
+*/
+int cIndex(int mappedIndex) {
+    return (mappedIndex + prevDataBufferHead) % numSamples;
+}
+
+int prevDataPush(int x, int y, int z) {
+    prevDataBufferHead = (prevDataBufferHead + 1) % numSamples;
+    prevDataX[prevDataBufferHead] = x;
+    prevDataY[prevDataBufferHead] = y;
+    prevDataZ[prevDataBufferHead] = z;
+}
+
+void print(ManagedString s) {
+    for (int i=0; i<s.length(); i++) {
+        uBit.serial.putc(s.charAt(i));
+    }
+}
 
 int main()
 {
     uBit.init();
 
+    print(ManagedString("Started!"));
+
+    smoothedX = uBit.accelerometer.getX() / 1000.0;
+    smoothedY = uBit.accelerometer.getY() / 1000.0;
+    smoothedZ = uBit.accelerometer.getZ() / 1000.0;
+
+    while(true) {
+        double x = uBit.accelerometer.getX() / 1000.0;
+        double y = uBit.accelerometer.getY() / 1000.0;
+        double z = uBit.accelerometer.getZ() / 1000.0;
+
+        smoothedX = smoothedX*0.75 + x*0.25;
+        smoothedY = smoothedY*0.75 + y*0.25;
+        smoothedZ = smoothedZ*0.75 + z*0.25;
+
+        prevDataPush(x,y,z);
+
+
+
+        uBit.sleep(1000 / samplesPerSecond);
+    }
+
     out_of_box_experience();
 
     microbit_panic( 999 );
 }
+
